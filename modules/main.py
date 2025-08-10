0ae644ad8747d8c2dabbee077bc8a589030f55c2
sameerkhan0009 <sk8860107@gmail.com>
2025-08-10 20:05:01 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -621,8 +621,8 @@ async def txt_handler(bot: Client, m: Message):
                     base_url = url.split("?")[0]
                     url = base_url.replace("https://static-db-v2.classx.co.in", "https://appx-content-v2.classx.co.in")
         
-            if "https://appx-transcoded-videos-mcdn.akamai.net.in/videos/neetkakajee-data/" in url:
-                url = url.replace("https://appx-transcoded-videos-mcdn.akamai.net.in/videos/neetkakajee-data/", "")
+            if "https://transcoded-videos.classx.co.in/videos/neetkakajee-data/" in url:
+                url = url.replace("https://transcoded-videos.classx.co.in/videos/neetkakajee-data/", "")
                 cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
         
     
