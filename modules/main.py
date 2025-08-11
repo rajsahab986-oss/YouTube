20724a48a041fbb7c6b5edcbb4dd83eb26f18ecc
sameerkhan0009 <sk8860107@gmail.com>
2025-08-11 21:08:12 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -630,8 +630,11 @@ async def txt_handler(bot: Client, m: Message):
                 
             if "https//appx1.arvind.eu.org/" in url:
                 url = url.replace("https://appx1.arvind.eu.org/","")
-                cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
-    
+                q = ((m3u8.loads(requests.get(url).text)).data['playlists'][1]['uri']).split("/")[0]
+                x = url.split("/")[5]
+                x = url.replace(x, "")
+                url = ((m3u8.loads(requests.get(url).text)).data['playlists'][1]['uri']).replace(q+"/", x)
+                
             if "youtu" in url:
                 ytf = f"b[height<={raw_text2}][ext=mp4]/bv[height<={raw_text2}][ext=mp4]+ba[ext=m4a]/b[ext=mp4]"
             else:
