2cfe5449b85a0b8e7ffd46458eb918e7bd5e816e
sameerkhan0009 <sk8860107@gmail.com>
2025-10-01 16:37:12 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -516,10 +516,12 @@ async def txt_handler(bot: Client, m: Message):
     await input6.delete(True)
     await editable.delete()
 
-    thumb = input6.text
-    if thumb.startswith("http://") or thumb.startswith("https://files.catbox.moe/mwhput.jpg"):
-        getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
-        thumb = "thumb.jpg"
+    if input6.photo:
+       thumb = await input6.download()
+    
+    elif raw_text6.startswith("http://") or raw_text6.startswith("https://"):
+        getstatusoutput(f"wget '{raw_text6}' -O 'thumb.jpg'")
+        thumb = "thumb.jpg"         
     else:
         thumb == "no"
 
